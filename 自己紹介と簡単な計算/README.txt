b5aec0e0c2bdb251e0820c20f0d3df21178e450e
fal1124 <38579564+fal1124@users.noreply.github.com>
2018-04-21 17:35:22 +0900

    Add files via upload

---
+++ 自己紹介と簡単な計算/README.txt
@@ -0,0 +1,2 @@
+javaをはじめてみた。
+System.out.println()と変数を使った簡単な計算をするCUIアプリケーションです。
---
+++ 自己紹介と簡単な計算/github0.java
@@ -0,0 +1,27 @@
+
+public class github0 {
+
+	public static void main(String[] args) {
+		//自己紹介
+		System.out.println("Hello World");
+		System.out.println("fal1124です。");
+		System.out.println("java初心者です。よろしくお願いします。");
+		
+		//計算
+		int x;
+		x=3;
+		
+		int tasuResult;
+		tasuResult=x+x;
+		
+		int hikuResult;
+		hikuResult=x-2;
+		
+		System.out.println("xを使った足し算の結果");
+		System.out.println(tasuResult);
+		
+		System.out.println("xを使った引き算の結果は");
+		System.out.println(hikuResult);
+	}
+
+}
