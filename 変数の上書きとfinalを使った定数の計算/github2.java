f3538768ed3e78f65905cfc84534e11335b24f87
fal1124 <38579564+fal1124@users.noreply.github.com>
2018-04-21 17:54:28 +0900

    Add files via upload

---
+++ 変数の上書きとfinalを使った定数の計算/github2.java
@@ -0,0 +1,13 @@
+
+public class github2 {
+	public static void main(String[] args) {
+		final double pi;
+		pi=3.16;
+		
+		System.out.println("半径6cmの円の面積を求めます。");
+		int hankei;
+		hankei=6;
+		System.out.println("結果は"+hankei*hankei*pi+"cm2");
+	}
+
+}
---
+++ 変数の上書きとfinalを使った定数の計算/guthub1.java
@@ -0,0 +1,12 @@
+
+public class guthub1 {
+	public static void main(String[] args) {
+		int num;
+		num=93;
+		System.out.println("„‚ÌD‚«‚È”š‚Í"+num);
+		
+		num=56;
+		System.out.println("‚Å‚àˆê”ÔD‚«‚È”š‚Í"+num);
+	}
+
+}
